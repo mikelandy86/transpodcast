edb4f7f789fa5e204639aeff4cc5cca9cf115b95
mikelandy86 <https://github.com/mikelandy86>
2023-04-09 00:41:02 +0200

    Added Header

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Hero from './Components/Hero.jsx'
+import Header from './Components/Header.jsx'
+
 
 function App() {
   return (
     <div className="App">
+       <Header/>
    <Hero/>
     </div>
   )
---
+++ src/Components/Header.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useRef } from 'react';
+
+const Header = () => {
+  const headerRef = useRef(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        headerRef.current.classList.add('scrolled');
+      } else {
+        headerRef.current.classList.remove('scrolled');
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <header className="header" ref={headerRef}>
+      <h1 className="header-logo">SEX SOM TRANS</h1>
+      <nav className="header-nav">
+        <a href="#listen" className="header-link">Lyssna nu!</a>
+        <a href="tel:07-7022520" className="header-link">07-7022520</a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
