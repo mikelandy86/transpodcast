6c847626cdf13979f8e4dfa15fc495df9d628227
mikelandy86 <https://github.com/mikelandy86>
2023-04-08 21:41:25 +0200

    Added Hero component

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Hero from './Components/Hero.jsx'
 
 function App() {
   return (
     <div className="App">
-   Test
+   <Hero/>
     </div>
   )
 }
