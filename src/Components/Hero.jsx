576860bb9bccdbf82d4e0061c5b6bcd7872ad762
mikelandy86 <https://github.com/mikelandy86>
2023-04-08 23:40:00 +0200

    Added Hero image and text

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -2,8 +2,18 @@ import React from 'react'
 
 export default function Hero() {
     return (
-        <div className="Hero">
-       HERO ART
-        </div>
+        <section className="Hero">
+       <div className='hero-container'>
+      <div className="hero-image">
+      <div className="image-overlay"></div>
+      <div className="hero-text">
+        <h1>SEX SOM TRANS</h1>
+        <p>en podd om sex, lust och relationer. Där du som lyssnare får följa med Mandy på sin resa och där vi tillsammans får lära oss om våra gränser och kanske hittar vi nya sidor hos oss själva. Lustfyllda möten och samtal som både kan bekräfta och utmana fantasin.</p>
+        <button>Lyssna nu!</button>
+      </div>
+      </div>
+   
+      </div>
+        </section>
       )
 }
