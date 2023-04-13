9bc049b86f1d3d8048ebb3f62b03cec57de3686b
mikelandy86 <https://github.com/mikelandy86>
2023-04-13 22:29:53 +0200

    Added Info section

--- src/Components/Info.jsx
+++ src/Components/Info.jsx
@@ -4,18 +4,18 @@ export default function Info() {
     return (
         <div className="info-section">
 <div className='info-container'>
-  <h2>Vad är Podplay?</h2>
-  <p>Podplay är Bauer Medias digitala podcastplattform och finns tillgänglig på podplay.com och som app till iPhone & Android. På Podplay finns ett stort urval av populära poddar inom bland annat </p>
+  <h2>Vem är jag?</h2>
+  <p>Jag är programledaren som ska vägleda er genom våra pirrande avsnitt. Jag har jobbat som fotograf/redigerare för flertalet tv-kanaler och produktionsbolag. Driver yttligare en podcast om drinkar och dryckeskultur som verkligen sätter guldkant på vardagen tillsammans med välkända gäster och blivit riktigt uppskattat av våra lyssnare.</p>
 
   </div>
   <div className='info-container'>
-  <h2>Vad kostar Podplay</h2>
-  <p>Att använda Podplay är helt gratis. På Podplay kan du lyssna på tusentals poddar, kostnadsfritt. Du kan också skapa ett </p>
+  <h2>Ge feedback</h2>
+  <p>Har upprättat ett telefonnummer till en röstbrevlåda för er lyssnare som vill lämna synpunkter, tankar eller egna erfarenheter efter varje avsnitt.</p>
 
   </div>
   <div className='info-container'>
-  <h2></h2>
-  <p>Har du en poddidé? Hos Bauer Media hittar du de erfarna specialisterna och den rätta tekniken som behövs för att göra framgångsrika podcasts. Vi hjälper dig att konceptutveckla, spela in och distribuera din podd. </p>
+  <h2>Bli en poddinsider</h2>
+<p>Anslut dig till vår växande poddfamilj och få en insiders titt på spännande ämnen, gäster och bakom kulisserna-insikter.</p>
 
   </div>
         </div>
