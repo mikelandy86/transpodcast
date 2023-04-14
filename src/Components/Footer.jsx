088718fe1b4ef4b5fd7f4d27300e3918bf709e2a
mikelandy86 <https://github.com/mikelandy86>
2023-04-14 23:46:24 +0200

    Added Info section

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,26 +3,18 @@ import React from 'react'
 export default function Footer() {
     return (
         <div className="footer">
-          <div className='footer-container'>
-
-       
 <div className="podstore-icon-container">
   <a>
   <img src='public/apple-appstore.svg'></img>
   </a>
   <a>
-  <img src='public/google-playstore.svg'></img>
+  <img src='google-playstore.svg'></img>
   </a>
 </div>
 <div className='bottom-container'>
-  <div className='socialmedia-container'>
-
-
-  <a><img src='public/facebook.svg'></img></a>
-  <a><img src='public/instagram.svg'></img></a>
-  </div>
-  <h3>PodWorld of Mandy Rich</h3>
-</div>
+  <a><img></img></a>
+  <a><img></img></a>
+  <h3>Mandy Rich PodWorld</h3>
 </div>
         </div>
       )
