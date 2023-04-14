0c738411f9b530be14d9367928b15f18b6bbced0
mikelandy86 <https://github.com/mikelandy86>
2023-04-14 23:39:15 +0200

    Added Info section

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -8,18 +8,18 @@ export default function Footer() {
        
 <div className="podstore-icon-container">
   <a>
-  <img src='./public/apple-appstore.svg'></img>
+  <img src='public/apple-appstore.svg'></img>
   </a>
   <a>
-  <img src='./public/google-playstore.svg'></img>
+  <img src='public/google-playstore.svg'></img>
   </a>
 </div>
 <div className='bottom-container'>
   <div className='socialmedia-container'>
 
 
-  <a><img src='./public/facebook.svg'></img></a>
-  <a><img src='./public/instagram.svg'></img></a>
+  <a><img src='public/facebook.svg'></img></a>
+  <a><img src='public/instagram.svg'></img></a>
   </div>
   <h3>PodWorld of Mandy Rich</h3>
 </div>
