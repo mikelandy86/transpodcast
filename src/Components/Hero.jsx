9602788ce5262b5592d7b0649bec719538d10083
mikelandy86 <https://github.com/mikelandy86>
2023-04-14 22:48:53 +0200

    Added Info section

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -9,7 +9,7 @@ export default function Hero() {
       <div className="hero-text">
         <h1>SEX SOM TRANS</h1>
         <h2>EN PODD FÖR DEN OBLYGA</h2>
-        <p>en podd om sex, lust och relationer. Där du som lyssnare får följa med Mandy på sin resa och där vi tillsammans får lära oss om våra gränser och kanske hittar vi nya sidor hos oss själva. Lustfyllda möten och samtal som både kan bekräfta och utmana fantasin.</p>
+        <p>Där du som lyssnare får följa med Mandy Rich på sin resa och där vi tillsammans får lära oss om våra gränser och kanske hittar vi nya sidor hos oss själva. Lustfyllda möten och samtal som både kan bekräfta och utmana fantasin.</p>
         <button>Lyssna nu!</button>
       </div>
       </div>
