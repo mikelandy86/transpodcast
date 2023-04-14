ce052d11712871c42a491a1bf50250356ae3c675
mikelandy86 <https://github.com/mikelandy86>
2023-04-14 23:48:44 +0200

    Added Footer Section

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,18 +3,26 @@ import React from 'react'
 export default function Footer() {
     return (
         <div className="footer">
+          <div className='footer-container'>
+
+       
 <div className="podstore-icon-container">
   <a>
-  <img src='public/apple-appstore.svg'></img>
+  <img src='apple-appstore.svg'></img>
   </a>
   <a>
   <img src='google-playstore.svg'></img>
   </a>
 </div>
 <div className='bottom-container'>
-  <a><img></img></a>
-  <a><img></img></a>
-  <h3>Mandy Rich PodWorld</h3>
+  <div className='socialmedia-container'>
+
+
+  <a><img src='facebook.svg'></img></a>
+  <a><img src='instagram.svg'></img></a>
+  </div>
+  <h3>PodWorld of Mandy Rich</h3>
+</div>
 </div>
         </div>
       )
