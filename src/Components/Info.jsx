54e646a90e622bf992d2115e7c294a2f57767ae0
mikelandy86 <https://github.com/mikelandy86>
2023-04-18 21:33:37 +0200

    added fade effect

--- src/Components/Info.jsx
+++ src/Components/Info.jsx
@@ -4,7 +4,7 @@ import { Fade } from "react-awesome-reveal";
 export default function Info() {
     return (
         <div className="info-section">
-          <Fade>
+          <Fade duration={1000} direction='left'>
 <div className='info-container'>
   <h2>Mer om podden</h2>
   <p>Sex som trans är en podcast som utforskar sex och sexualitet från ett transperspektiv. Mandy Rich delar med sig av sina personliga erfarenheter och bjuder in gäster för att diskutera mer djupgående. Podden syftar till att öppna upp en dialog kring sex och relationer, och ge lyssnarna mer kunskap.</p>
