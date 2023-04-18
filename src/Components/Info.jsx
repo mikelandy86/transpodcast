b18c77dffdfca6ee4298b9c87820ea5dc42ae4d5
mikelandy86 <https://github.com/mikelandy86>
2023-04-18 21:29:35 +0200

    added fade effect

--- src/Components/Info.jsx
+++ src/Components/Info.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import { Fade } from "react-awesome-reveal";
 
 export default function Info() {
     return (
         <div className="info-section">
+          <Fade>
 <div className='info-container'>
-  <h2>Vem är jag?</h2>
-  <p>Jag är programledaren som ska vägleda er genom våra pirrande avsnitt. Jag har jobbat som fotograf/redigerare för flertalet tv-kanaler och produktionsbolag. Driver yttligare en podcast om drinkar och dryckeskultur som verkligen sätter guldkant på vardagen tillsammans med välkända gäster och blivit riktigt uppskattat av våra lyssnare.</p>
+  <h2>Mer om podden</h2>
+  <p>Sex som trans är en podcast som utforskar sex och sexualitet från ett transperspektiv. Mandy Rich delar med sig av sina personliga erfarenheter och bjuder in gäster för att diskutera mer djupgående. Podden syftar till att öppna upp en dialog kring sex och relationer, och ge lyssnarna mer kunskap.</p>
 
   </div>
   <div className='info-container'>
@@ -15,8 +17,9 @@ export default function Info() {
   </div>
   <div className='info-container'>
   <h2>Kontakt</h2>
-  <p>Har du frågor, funderingar eller vill kommma med förslag på gäster? Hör av dig till mig på <a href="mailto:sexsomtrans@gmail.com?Subject=&Body=" title="Hello" >sexsomtrans@gmail.com</a></p>
+  <p>Har du frågor, funderingar eller vill komma med förslag på gäster? Hör av dig till mig på <a href="mailto:sexsomtrans@gmail.com?Subject=&Body=" title="Hello" >sexsomtrans@gmail.com</a></p>
   </div>
+  </Fade>
         </div>
       )
 }
